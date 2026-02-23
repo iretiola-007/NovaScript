2e023112a0cb89fa1207ccfd6e41502462fc4125
Iretiola <abisigairetiola@gmail.com>
2026-02-23 18:25:02 +0100

    Create languages/german.py
    Initial language pack for German language

---
+++ novascript/languages/german.py
@@ -0,0 +1,15 @@
+KEYWORDS = {
+    "variable": "VAR",
+    "ausgabe": "PRINT",
+    "wenn": "IF",
+    "funktion": "FUNC",
+    "zurueck": "RETURN",
+    "sprache": "LANGUAGE", 
+    "wenn": "IF",
+    "sonst": "ELSE",
+}
+
+ERRORS = {
+    "SYNTAX": "Syntaxfehler",
+    "NAME": "Namensfehler"
+}
